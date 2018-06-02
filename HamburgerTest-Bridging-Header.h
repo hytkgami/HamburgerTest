2497567037adf5b003a7f8b1738e163b152c1ff1
hytkgami <gagagatrump@gmail.com>
2018-06-02 18:17:49 +0900

    WSCoachMarksViewの導入

---
+++ HamburgerTest-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HamburgerTest-Bridging-Header.h
+//  HamburgerTest
+//
+//  Created by Hajime Kagami on 2018/06/02.
+//  Copyright © 2018年 Hajime Kagami. All rights reserved.
+//
+
+#ifndef HamburgerTest_Bridging_Header_h
+#define HamburgerTest_Bridging_Header_h
+
+#import "WSCoachMarksView.h"
+
+#endif /* HamburgerTest_Bridging_Header_h */
--- HamburgerTest/ViewController.swift
+++ HamburgerTest/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import WSCoachMarksView
+import Foundation
+import QuartzCore
 
 class ViewController: UIViewController {
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let f = self.view.bounds
+        let arrCoach = [
+            [
+                "rect" : CGRect(x: 4.0, y: 22.0, width: 45, height: 45),
+                "caption" : "ここをタップすると\nメニューが開きます",
+                "shape" : "circle"
+            ],
+        ]
+        guard let coach = WSCoachMarksView(frame: f, coachMarks: arrCoach) else {
+            return
+        }
+        coach.maskColor = UIColor(white: 0.0, alpha: 0.65)
+        self.view.addSubview(coach)
+        self.view.bringSubview(toFront: coach)
+        coach.start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +42,5 @@ class ViewController: UIViewController {
     @IBAction func tapMenu(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "showMenu", sender: nil)
     }
-
 }
 
